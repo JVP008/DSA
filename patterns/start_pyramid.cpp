3e4f4254924b53ee31cd2979de3d62a1d264410a
Jayesh <jayeshpatil20081234@gmail.com>
2026-06-28 18:27:59 +0530

    :)

--- patterns/start_pyramid.cpp
+++ patterns/start_pyramid.cpp
@@ -1,18 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+Pattern:
+    *
+   * *
+  * * *
+ * * * *
+*/
 int main()
 {
-    // int n;
-    // cout<<"Enter N: ";
-    // cin>>n;
-    
-    for (int i = 1;i<=10;i+2){
-        for (int j=1;j<=i;j++){
-        cout<<"*";
+
+    int n;
+    cout<<"Enter N: ";
+    cin>>n;
+
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+        for (int j = 0;j<i;j++){
+            cout<<"* ";
         }
         cout<<endl;
     }
+
+
     
     return 0;
 }
