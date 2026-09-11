bdb6bdffa83568554c2cbb9063f7f2cdb591246b
JVP008 <jayeshpatil20081234@gmail.com>
2026-09-11 13:38:39 +0000

    updt: sorry for n-1 :)

--- sorting/Insertion.cpp
+++ sorting/Insertion.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 // suppose we have arr = [1,3,4,2,5] -> [1,2,3,4,5];
 // In the insertion sort we kind of maintain a window (not the window with two pointers! ok).
-// the moment we move forward to the right windows get bigger and bigger (ofc) and we
+// the moment we move forward to the right, window gets bigger and bigger (ofc) and we
 // then try to insert the elements which is in transit of getting placed in that window and for that
 // we do following things (conditions):
-// if the the element to be insert is bigger than then we shift the elements to the right and
+// if the the element to be insert is bigger then we shift the elements to the right and
 // we compare the element_to_be_inserted with its immediate left till the left becomes smaller than
 // the element to be inserted.
 // for example : [1,3,4] is already in sorted manner and the our next element which is not part
@@ -22,9 +22,9 @@ int main()
         cin>>arr[i];
     }
     //logic
-    for (int i=0;i<n-1;i++){ // going till n-1 so that we can compare last element with second last
+    for (int i=1;i<n;i++){ 
         int j = i;
-        while (j>0 && arr[j-1]>arr[j]){
+        while (j>0 && arr[j-1]>arr[j]){ // going till n-1 so that we can compare last element with second last
             int temp = arr[j];
             arr[j]= arr[j-1];
             arr[j-1] = temp;
---
+++ sorting/recursive_insertion.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void insertion_sort(int arr[],int n,int i){
+    //base condition
+    if (i==n){
+        return ;
+    }
+    int j = i;
+    while (j>0 && arr[j]<arr[j-1]){
+        int temp = arr[j];
+        arr[j] = arr[j-1];
+        arr[j-1] = temp;
+        j-=1;
+    }
+    i+=1;
+    insertion_sort(arr,n,i+1);
+
+}
+
+int main(){
+
+    int arr[10] = {10,9,8,7,6,5,3,4,2,1};
+    int n = 10;
+    insertion_sort(arr,n,1);
+    for (int i :arr){
+        cout<<i<<" ";
+    }
+    return 0;
+}
